d748989e323f25e337c1ff7230ca32ff9ae4e637
Suthirtha Dey <suthirthasoma@gmail.com>
2025-05-29 13:45:26 +0530

    Edited App.jsx in Vite React

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,11 +1,15 @@
-
+import  First from "./first"
 
 
 function App() {
   
 
-  return (
-    <h1> React with Run</h1>
+  return ( //This <> </> is called fragment since a single component can be passed through App.jsx so we nedd to wrap it in a single component.
+    <>
+      <h1>React with Vite</h1>
+     <First/>
+      <p>Second React Practice</p>
+    </>
   )
 }
 
