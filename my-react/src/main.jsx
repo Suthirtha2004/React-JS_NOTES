dc53aa030a84a755d423b062ec7bc87362793752
Suthirtha Dey <suthirthasoma@gmail.com>
2025-05-29 22:06:14 +0530

    Edited

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -3,8 +3,34 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1> Custom App</h1>
+    </div>
+  )
+}
+
+// const reactElement = { 
+//     type : 'a',
+//     props : { 
+//         href : 'https://www.google.com',
+//         target : '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href = "www.google.com" target = '_blank' >Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href : 'https://www.google.com'}
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    anotherElement
+ 
 )
