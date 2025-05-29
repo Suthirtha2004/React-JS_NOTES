c36759c36a0e0e22af4d8b33d6870480969dab7c
Suthirtha Dey <suthirthasoma@gmail.com>
2025-05-29 13:44:53 +0530

    Edited index file in 01Basics

--- 01basicsreact/src/index.js
+++ 01basicsreact/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( //A variable name root is made.React DOM is the React to be used in the website. Render the root
   <React.StrictMode>
     <App />
   </React.StrictMode>
